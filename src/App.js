e0217fcb63b7602116132b606c012cf6cbb880ad
Mike Schmidt <schmidt217@gmail.com>
2021-09-23 15:04:31 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import React, { Component } from "react";
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Navbar />
+			</div>
+		);
+	}
+}
+
+export default App;
